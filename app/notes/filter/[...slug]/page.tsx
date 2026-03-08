6b0c6be30b5cedf8a08f48f7a3144616e431b4ea
Bozhenko Zakhar <zakhar1bozhenko@gmail.com>
2026-03-08 09:44:08 +0100

    Add the main metatags

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,10 +1,32 @@
 import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query';
+import { Metadata } from 'next';
 import { fetchNotes, fetchNotesByCategory } from '@/lib/api';
 import NotesClient from './Notes.client';
 import { NoteTag } from '@/types/note';
 
 type Props = {
 	params: Promise<{slug: string[]}>;
+}
+
+export async function generateMetadata({params}: Props): Promise<Metadata> {
+	const { slug } = await params;
+	const category = slug[0] === 'all' ? undefined : slug[0] as NoteTag
+
+	return {
+		title: category ? `NoteHub ${category} Notes` : undefined,
+		description: `NoteHub application with filtered notes by «${category}» category`,
+		openGraph: {
+			title: category ? `NoteHub ${category} Notes` : undefined,
+			description: `NoteHub application with filtered notes by «${category}» category`,
+			url: `https://08-zustand-sigma-rosy.vercel.app/notes/${category}`,
+			images: [{
+				alt: "NoteHub preview image",
+				width: "1200",
+				height: "630",
+				url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg"
+			}]
+		}
+	}
 }
 
 export default async function Notes({params}: Props) {
